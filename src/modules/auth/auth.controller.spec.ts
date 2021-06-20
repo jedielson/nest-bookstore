ee55d6fea5e592b1a87ecab7e16e49edde2fbe56
Jedi <jedielson.jn@outlook.com>
2021-06-20 18:21:48 -0300

    Testing AuthController

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,16 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { DoesUserExistGuard } from '../../core/guards/does-user-exist.guard';
+import { CreateUserResponse, UserDto } from '../users/dto/user.dto';
 import { UsersService } from '../users/users.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
           provide: UsersService,
+          useValue: {},
+        },
+        {
+          provide: DoesUserExistGuard,
           useValue: {},
         },
         {
@@ -22,9 +29,33 @@ describe('AuthController', () => {
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('login should return a user', async () => {
+    // arrange
+    service.login = jest.fn().mockResolvedValueOnce('some return');
+
+    // act & assert
+    await expect(controller.login({ user: '' })).resolves.toBe('some return');
+  });
+
+  it('signup should return a user', async () => {
+    // arrange
+    const userResponse: CreateUserResponse = {
+      user: {
+        name: 'some-name',
+        email: 'a@b.com',
+      },
+      token: 'some token',
+    };
+    service.create = jest.fn().mockResolvedValueOnce(userResponse);
+
+    // act & assert
+    await expect(controller.signUp(new UserDto())).resolves.toBe(userResponse);
   });
 });
