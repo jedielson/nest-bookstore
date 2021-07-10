ec1024ef737d2c759fb750134079cb0284b59555
Jedi <jedielson.jn@outlook.com>
2021-07-10 20:53:41 -0300

    fixing interceptor test

--- src/core/http/location.interceptor.spec.ts
+++ src/core/http/location.interceptor.spec.ts
@@ -1,7 +1,19 @@
 import { LocationInterceptor } from './location.interceptor';
+import { Reflector } from '@nestjs/core';
+import { Test, TestingModule } from '@nestjs/testing';
 
 describe('LocationInterceptor', () => {
+  let interceptor: LocationInterceptor;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [Reflector],
+    }).compile();
+
+    const reflector = module.get<Reflector>(Reflector);
+    interceptor = new LocationInterceptor(reflector);
+  });
+
   it('should be defined', () => {
-    expect(new LocationInterceptor()).toBeDefined();
+    expect(interceptor).toBeDefined();
   });
 });
